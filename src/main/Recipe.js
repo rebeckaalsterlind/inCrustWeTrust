450d6b70be29cc4231e9aa14e8723ad353f29330
Rebecka Alsterlind <[rebecka.alsterlind@gmail.com]>
2022-07-18 13:02:00 +0200

    mapped options/select in Calculate render instead, lifted recipe() to new file

--- src/main/Recipe.js
+++ src/main/Recipe.js
@@ -4,17 +4,24 @@ import './recipe.css';
 function Recipe({weight, ingredients, total}) {
   
   return (
-    <section>
+    <section className="recipe">
+      <h3>Recipe</h3>
       <ul>
         {weight.length !== 0 && weight.map((i, index) => (
           i.amount > 0 && 
-            <li key={index}>{i.name.charAt(0).toUpperCase() + i.name.slice(1)}: {Math.round(i.amount*10)/10}g</li>
+            <li key={index}>{i.name.charAt(0).toUpperCase() + i.name.slice(1)}: {Math.round(i.amount*10)/10}
+            {i.name !== "doughballs" && "g"}</li>
         ))} 
-       {total && <p>Total weight: {total}</p>}
+       {total && <p>Total weight: {total}g</p>}
         <br />
         {ingredients.length !== 0 && ingredients.map((i, index) => (
           i.amount > 0 && 
-            <li key={index}>{i.name.charAt(0).toUpperCase() + i.name.slice(1)}: {Math.round(i.amount*10)/10}g</li>
+            <li key={index}>
+              {i.name.charAt(0).toUpperCase() + i.name.slice(1)}:&nbsp;
+              {i.name === "flour"|| i.name === "water" 
+              ?  Math.floor(i.amount) 
+              : Math.round(i.amount*10)/10}g
+            </li>
         ))}
       </ul>
     </section>
